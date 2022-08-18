44fe32f2024ec76bc83f34c5da4053e1ae7fee82
Shannon Brooks <sbrooks0084@gmail.com>
2022-08-18 17:43:06 -0500

    completed

--- petServer.js
+++ petServer.js
@@ -1,22 +1,56 @@
-const http = require('http'); //grab module and store in variable
-const fs = require('fs')  //grabs file share module and stores in var
-const port = 7777  //this is our door to the data
+// 
 
-const server = http.createServer();  //from http var, create server - aka: localhost
+const http = require('http');
+const fs = require('fs')
+const port = 7777
 
-server.on('request', function (req, res) {  //handles the request and response; allows request and grabs data from diff files
-    
-    if (req.url === '/pets') {     //request pets endpoint from url
-        fs.readFile('pets.json', 'utf-8', function (err, data) {   //use fs.readFile to grab
+const server = http.createServer();
+
+
+server.on('request', function (req, res) {
+    // res.end('something new')
+
+    const items = req.url.split('/');
+
+    if (items.length > 2) {
+        fs.readFile('pets.json', 'utf-8', function (err, data) {
             if (err) {
                 console.log("trash")
                 process.exit(1)
             }
 
-            let petData = data
+            const petData = JSON.parse(data)
+            const index = items[items.length - 1]
+            console.log(petData)
+
+            if (index < 0) {
+                res.status = 404
+                res.setHeader('Content-Type', 'text/plain')
+                res.end('Not Found')
+            } else if (index >= petData.length) {
+                res.status = 404
+                res.setHeader('Content-Type', 'text/plain')
+                res.end('Not Found')
+            } else {
+                res.status = 200
+                res.setHeader('Content-Type', 'application/json')
+                const elem = petData[Number(index)]
+                res.end(JSON.stringify(elem))
+            }
+        })
+    }
+
+    if (req.url === '/pets') {
+        fs.readFile('pets.json', 'utf-8', function (err, data) {
+            if (err) {
+                console.log("trash")
+                process.exit(1)
+            }
+
+
             res.status = 200;
             res.setHeader('Content-type', 'application/json')
-            res.end(JSON.stringify(petData))
+            res.end(data)
         })
 
     }
